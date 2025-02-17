5eb3472f889289e6d1983f591d09e99f334ab8b3
samarareddy <samara.junna@gmail.com>
2025-02-17 10:12:52 +0530

    some-message

---
+++ 10-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+
+echo "Before calling other script, couse: $COURSE"
+echo "Process ID of current shell script: $$"
+
+#./09-other-script.sh
+
+echo "After calling other script, couse: $COURSE"
